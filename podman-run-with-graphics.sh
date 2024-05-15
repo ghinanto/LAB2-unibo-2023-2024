83af2c18be7ac6a4575a074fae3c9f5a2d1ec98b
ghinanto <antonioghin@tutanota.com>
2024-05-15 15:24:14 +0200

    Add comments in script

--- podman-run-with-graphics.sh
+++ podman-run-with-graphics.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# Add container name at the end and eventually command to run in container at the very end.
+# ex. podman-run-with-graphics.sh rootproject/root
+# ex. podman-run-with-graphics.sh rootproject/root "root --web=off"
 podman run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --rm -it -v $HOME:/userhome --user $(id -u) $*
