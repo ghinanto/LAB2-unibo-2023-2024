0aa58e44e15ea7b1f1dc479d451efd5fd76b72c1
ghinanto <antonioghin@tutanota.com>
2024-06-17 17:25:10 +0200

    modified podman mount folder

--- podman-run-with-graphics.sh
+++ podman-run-with-graphics.sh
@@ -2,4 +2,4 @@
 # Add container name at the end and eventually command to run in container at the very end.
 # ex. podman-run-with-graphics.sh rootproject/root
 # ex. podman-run-with-graphics.sh rootproject/root "root --web=off"
-podman run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --rm -it -v $HOME:/userhome --user $(id -u) $*
+podman run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --rm -it -v $HOME:/opt/userhome $*
